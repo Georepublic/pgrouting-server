334d3507ac9154ab42c51befd7b8e038f3107230
Mario Basa <mario.basa@gmail.com>
2012-02-10 00:00:24 +0900

    added priority slider in configuration section

--- WebContent/resources/js/controlpage.js
+++ WebContent/resources/js/controlpage.js
@@ -131,30 +131,55 @@ controlService.prototype.clearParams = function() {
 	
 	$("#motorway_ck").attr('checked',false);
 	$("#motorway_sp").val("0");
+	$("#motorway_sl").slider({value:1});
+	
 	$("#motorwaylnk_ck").attr('checked',false);
 	$("#motorwaylnk_sp").val("0");
+	$("#motorwaylnk_sl").slider({value:1});
+	
 	$("#trunkway_ck").attr('checked',false);
 	$("#trunkway_sp").val("0");
+	$("#trunkway_sl").slider({value:1});
+	
 	$("#trunkwaylnk_ck").attr('checked',false);
 	$("#trunkwaylnk_sp").val("0");
+	$("#trunkwaylnk_sl").val("0");
+	
 	$("#primary_ck").attr('checked',false);
 	$("#primary_sp").val("0");
+	$("#primary_sl").slider({value:1});
+	
 	$("#primarylnk_ck").attr('checked',false);
 	$("#primarylnk_sp").val("0");
+	$("#primarylnk_sl").slider({value:1});
+	
 	$("#secondary_ck").attr('checked',false);
 	$("#secondary_sp").val("0");
+	$("#secondary_sl").slider({value:1});
+	
 	$("#secondarylnk_ck").attr('checked',false);
 	$("#secondarylnk_sp").val("0");
+	$("#secondarylnk_sl").slider({value:1});
+	
 	$("#tertiary_ck").attr('checked',false);
 	$("#tertiary_sp").val("0");
+	$("#tertiary_sl").slider({value:1});
+	
 	$("#residential_ck").attr('checked',false);
 	$("#residential_sp").val("0");
+	$("#residential_sl").slider({value:1});
+	
 	$("#unclassified_ck").attr('checked',false);
 	$("#unclassified_sp").val("0");
+	$("#unclassified_sl").slider({value:1});
+	
 	$("#pedestrian_ck").attr('checked',false);
 	$("#pedestrian_sp").val("0");
+	$("#pedestrian_sl").slider({value:1});
+	
 	$("#cycleway_ck").attr('checked',false);
 	$("#cycleway_sp").val("0");
+	$("#cycleway_sl").slider({value:1});
 };
 
 controlService.prototype.setProfileVals = function( id ) {
@@ -184,54 +209,67 @@ controlService.prototype.setProfileVals = function( id ) {
 					case 11: 
 						$("#motorway_ck").attr('checked',config[j].enabled);
 						$("#motorway_sp").val(config[j].speed);
+						$("#motorway_sl").slider({value:config[j].priority});
 						break;
 					case 12: 
 						$("#motorwaylnk_ck").attr('checked',config[j].enabled);
 						$("#motorwaylnk_sp").val(config[j].speed);
+						$("#motorwaylnk_sl").slider({value:config[j].priority});
 						break;
 					case 13: 
 						$("#trunkway_ck").attr('checked',config[j].enabled);
 						$("#trunkway_sp").val(config[j].speed);
+						$("#trunkway_sl").slider({value:config[j].priority});
 						break;
 					case 14: 
 						$("#trunkwaylnk_ck").attr('checked',config[j].enabled);
 						$("#trunkwaylnk_sp").val(config[j].speed);
+						$("#trunkwaylnk_sl").slider({value:config[j].priority});
 						break;
 					case 15: 
 						$("#primary_ck").attr('checked',config[j].enabled);
 						$("#primary_sp").val(config[j].speed);
+						$("#primary_sl").slider({value:config[j].priority});
 						break;
 					case 16: 
 						$("#primarylnk_ck").attr('checked',config[j].enabled);
 						$("#primarylnk_sp").val(config[j].speed);
+						$("#primarylnk_sl").slider({value:config[j].priority});
 						break;
 					case 21: 
 						$("#secondary_ck").attr('checked',config[j].enabled);
 						$("#secondary_sp").val(config[j].speed);
+						$("#secondary_sl").slider({value:config[j].priority});
 						break;
 					case 22: 
 						$("#secondarylnk_ck").attr('checked',config[j].enabled);
 						$("#secondarylnk_sp").val(config[j].speed);
+						$("#secondarylnk_sl").slider({value:config[j].priority});
 						break;
 					case 31: 
 						$("#tertiary_ck").attr('checked',config[j].enabled);
 						$("#tertiary_sp").val(config[j].speed);
+						$("#tertiary_sl").slider({value:config[j].priority});
 						break;
 					case 32: 
 						$("#residential_ck").attr('checked',config[j].enabled);
 						$("#residential_sp").val(config[j].speed);
+						$("#residential_sl").slider({value:config[j].priority});
 						break;
 					case 42: 
 						$("#unclassified_ck").attr('checked',config[j].enabled);
 						$("#unclassified_sp").val(config[j].speed);
+						$("#unclassified_sl").slider({value:config[j].priority});
 						break;
 					case 62: 
 						$("#pedestrian_ck").attr('checked',config[j].enabled);
 						$("#pedestrian_sp").val(config[j].speed);
+						$("#pedestrian_sl").slider({value:config[j].priority});
 						break;
 					case 81: 
 						$("#cycleway_ck").attr('checked',config[j].enabled);
 						$("#cycleway_sp").val(config[j].speed);
+						$("#cycleway_sl").slider({value:config[j].priority});
 						break;
 					}
 				}
@@ -274,31 +312,56 @@ controlService.prototype.saveParams = function() {
 	opts+='"id": 0,';
 	opts+='"rid":'+resourceId+'},"configuration":[';
 	
-	opts+='{"cid":11,"priority":1,"speed":'+$("#motorway_sp").val()+
+	opts+='{"cid":11,"priority":'+ $("#motorway_sl").slider("value") +
+		',"speed":'+$("#motorway_sp").val()+
 		',"enabled":"'+$("#motorway_ck").prop("checked")+'"},';
-	opts+='{"cid":12,"priority":1,"speed":'+$("#motorwaylnk_sp").val()+
+	
+	opts+='{"cid":12,"priority":'+ $("#motorwaylnk_sl").slider("value") +
+		',"speed":'+$("#motorwaylnk_sp").val()+
 		',"enabled":"'+$("#motorwaylnk_ck").prop("checked")+'"},';
-	opts+='{"cid":13,"priority":1,"speed":'+$("#trunkway_sp").val()+
+	
+	opts+='{"cid":13,"priority":'+ $("#trunkway_sl").slider("value") +
+		',"speed":'+$("#trunkway_sp").val()+
 		',"enabled":"'+$("#trunkway_ck").prop("checked")+'"},';
-	opts+='{"cid":14,"priority":1,"speed":'+$("#trunkwaylnk_sp").val()+
+	
+	opts+='{"cid":14,"priority":'+ $("#trunkwaylnk_sl").slider("value") +
+		',"speed":'+$("#trunkwaylnk_sp").val()+
 		',"enabled":"'+$("#trunkwaylnk_ck").prop("checked")+'"},';
-	opts+='{"cid":15,"priority":1,"speed":'+$("#primary_sp").val()+
+	
+	opts+='{"cid":15,"priority":'+ $("#primary_sl").slider("value") +
+		',"speed":'+$("#primary_sp").val()+
 		',"enabled":"'+$("#primary_ck").prop("checked")+'"},';
-	opts+='{"cid":16,"priority":1,"speed":'+$("#primarylnk_sp").val()+
+	
+	opts+='{"cid":16,"priority":'+ $("#primarylnk_sl").slider("value") +
+		',"speed":'+$("#primarylnk_sp").val()+
 		',"enabled":"'+$("#primarylnk_ck").prop("checked")+'"},';
-	opts+='{"cid":21,"priority":1,"speed":'+$("#secondary_sp").val()+
+	
+	opts+='{"cid":21,"priority":'+ $("#secondary_sl").slider("value") +
+		',"speed":'+$("#secondary_sp").val()+
 		',"enabled":"'+$("#secondary_ck").prop("checked")+'"},';
-	opts+='{"cid":22,"priority":1,"speed":'+$("#secondarylnk_sp").val()+
+	
+	opts+='{"cid":22,"priority":'+ $("#secondarylnk_sl").slider("value") +
+		',"speed":'+$("#secondarylnk_sp").val()+
 		',"enabled":"'+$("#secondarylnk_ck").prop("checked")+'"},';
-	opts+='{"cid":31,"priority":1,"speed":'+$("#tertiary_sp").val()+
+	
+	opts+='{"cid":31,"priority":'+ $("#tertiary_sl").slider("value") +
+		',"speed":'+$("#tertiary_sp").val()+
 		',"enabled":"'+$("#tertiary_ck").prop("checked")+'"},';
-	opts+='{"cid":32,"priority":1,"speed":'+$("#residential_sp").val()+
+	
+	opts+='{"cid":32,"priority":'+ $("#residential_sl").slider("value") +
+		',"speed":'+$("#residential_sp").val()+
 		',"enabled":"'+$("#residential_ck").prop("checked")+'"},';
-	opts+='{"cid":42,"priority":1,"speed":'+$("#unclassified_sp").val()+
+	
+	opts+='{"cid":42,"priority":'+ $("#unclassified_sl").slider("value") +
+		',"speed":'+$("#unclassified_sp").val()+
 		',"enabled":"'+$("#unclassified_ck").prop("checked")+'"},';
-	opts+='{"cid":62,"priority":1,"speed":'+$("#pedestrian_sp").val()+
+	
+	opts+='{"cid":62,"priority":'+ $("#pedestrian_sl").slider("value") +
+		',"speed":'+$("#pedestrian_sp").val()+
 		',"enabled":"'+$("#pedestrian_ck").prop("checked")+'"},';
-	opts+='{"cid":81,"priority":1,"speed":'+$("#cycleway_sp").val()+
+	
+	opts+='{"cid":81,"priority":'+ $("#cycleway_sl").slider("value") +
+		',"speed":'+$("#cycleway_sp").val()+
 		',"enabled":"'+$("#cycleway_ck").prop("checked")+'"}';
 	
 	var mHosts = $("#hosts").val().split('\n');
